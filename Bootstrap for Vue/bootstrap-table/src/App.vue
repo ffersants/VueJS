7085cb7be370383efa214ed0b3249c5564c1ec47
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-02-21 16:19:03 -0300

    First display of the table with <b-table> and the directive :items, using the response from the route GET /users/ in Siscoffee project.

---
+++ Bootstrap for Vue/bootstrap-table/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: { HelloWorld },
+  name: 'App',
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Bootstrap for Vue/bootstrap-table/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <b-table :items="items">
+
+    </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      items: ""
+    }
+  },
+  beforeCreate: function(){
+    fetch("http://localhost:3300/users/")
+      .then(r => r.json())
+      .then((r) => {
+        this.items = r;
+      })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
